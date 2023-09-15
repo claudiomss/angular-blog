8c7aac9e4dc00aaba7d9ab3413c810cdf8bfb2a5
claudiomss <claudiocld95@gmail.com>
2023-09-15 03:32:06 -0300

    Em desenvolvimento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Card1Component } from './components/card1/card1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Card1Component
   ],
   imports: [
     BrowserModule,
